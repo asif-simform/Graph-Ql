85cd9ca8a75d1c45e6fcd8d96108a737d3ae9594
asif vora <asif.v@simformsolutions.com>
2022-07-04 16:54:18 +0530

    Setup react

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div class="container mx-auto px-4">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
